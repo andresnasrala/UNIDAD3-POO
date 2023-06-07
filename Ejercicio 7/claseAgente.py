10b18cca9aca39d98100dae80bfa7566782039eb
Andrés Nasrala <128121242+andresnasrala@users.noreply.github.com>
2023-06-07 19:43:18 -0300

    Add files via upload

---
+++ Ejercicio 7/claseAgente.py
@@ -0,0 +1,30 @@
+class Agente:
+    __cuil : str 
+    __apellido : str 
+    __nombre : str 
+    __sueldo_basico : float 
+    __antiguedad : str 
+    def __init__(self, cuil, apellido, nombre, sueldo_basico, antiguedad):
+        self.__cuil = cuil
+        self.__apellido = apellido
+        self.__nombre = nombre
+        self.__sueldo_basico = sueldo_basico
+        self.__antiguedad = antiguedad
+   
+    def getCuil(self):
+        return self.__cuil
+    
+    def getApellido(self):
+        return self.__apellido
+    
+    def getNombre(self):
+        return self.__nombre
+    
+    def getSueldo(self):
+        return self.__sueldo_basico
+    
+    def getAntiguedad(self):
+        return self.__antiguedad   
+    
+    def getApellidoyNombre(self):
+        return self.__apellido+" "+self.__nombre
---
+++ Ejercicio 7/claseDocenteInvestigador.py
@@ -0,0 +1,30 @@
+from claseDocente import Docente
+from claseInvestigador import Investigador
+
+class DocenteInvestigador(Docente, Investigador):
+    def __init__(self, cuil, apellido, nombre, sueldo_basico, antiguedad, carrera, cargo, catedra, area_investigacion, tipo_investigacion, categoria, importe_extra):
+        super().__init__(cuil, apellido, nombre, sueldo_basico, antiguedad, carrera, cargo, catedra)
+        super().__init__(cuil, apellido, nombre, sueldo_basico, antiguedad, area_investigacion, tipo_investigacion)
+        self.__categoria = categoria
+        self.__importe_extra = importe_extra
+
+    def __str__(self):
+        return f"{super().__str__()}, {super().__str__()}, {self.__categoria}, {self.__importe_extra}"
+    
+    def getCategoria(self):
+        return self.__categoria
+    
+    def getImporteExtra(self):
+        return self.__importe_extra
+    
+    def getCategoriaPrograma(self):
+        return self.__categoría_programa
+    
+    def to_dict(self):
+        base_dict = super(Docente, self).to_dict()  
+        base_dict.update(super(Investigador, self).to_dict())  
+        base_dict.update({
+            "categoría_programa": self.get_categoria_programa(),
+            "importe_extra": self.get_importe_extra(),
+        })
+        return base_dict
